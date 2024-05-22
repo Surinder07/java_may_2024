78a42615bcebdf2477138750e831041a7f7e35fa
surinder0322@gmail.com <sunny@sunnys-laptop.home>
2024-05-21 21:56:42 -0400

    more

--- src/may21/NumberDemo.java
+++ src/may21/NumberDemo.java
@@ -12,18 +12,18 @@ public class NumberDemo {
         int num = scanner.nextInt();
 
         //    -3  -2  - 1 0  1 2 3 4 5
-        boolean result = num >= 0;
+       // boolean result = num >= 0;
 
-        if(result) {
-            System.out.println("Number is positive");
+        if(num % 2 == 0) {
+            System.out.println("Number is even");
         }
         else{
-            System.out.println("Number is negative..");
+            System.out.println("Number is odd..");
         }
 
     }
 }
-// odd or even ? %
+// odd or even ? %   - (num % 2 == 0)
 
 // 1. check if the given number is even/odd
 // 2. given number x = 10, y = 20, check which one is greater...
