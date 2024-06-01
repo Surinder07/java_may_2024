f13e59f3aeda80fb1be3c094d93cc1a8897f0038
surinder0322@gmail.com <sunny@sunnys-laptop.lan>
2024-06-01 12:17:19 -0400

    method overloading

---
+++ src/june1/NameInput.java
@@ -0,0 +1,25 @@
+package june1;
+
+public class NameInput {
+
+
+    public String input(String name ){
+
+        // ask use to enter the name and return the name
+        // hello name
+        return "name ";
+    }
+
+
+}
+
+/*
+
+create a method ,
+String input(String)  -- name
+input(String, String ) -- name, email
+input(String, String , String ) -- name, email, password.
+
+
+
+ */
