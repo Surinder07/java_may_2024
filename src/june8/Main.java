7980eb1274255a74b3936864495608a448503ff0
surinder0322@gmail.com <sunny@sunnys-laptop.home>
2024-06-09 00:36:30 -0400

    assignments

--- src/june8/Main.java
+++ src/june8/Main.java
@@ -6,17 +6,32 @@ public class Main {
     public static void main(String[] args) {
         int[] arr = {10,20,30,40,50};
 
-        System.out.println(showArray(arr));
      //   Arrays.toString(arr)
-
 
     }
 
-    public static int[] showArray(int[] arr){
-
+    public static void showArray(int[] arr){
         // logic
 
-        return arr;
+
     }
 
 }
+
+/*
+
+int max = 0;
+
+if(max < arr[i]){
+
+max = arr[i]
+}
+
+
+
+
+
+ */
+
+
+
---
+++ src/june8/TwoDimArrayDemo.java
@@ -0,0 +1,27 @@
+package june8;
+
+public class TwoDimArrayDemo {
+    public static void main(String[] args) {
+
+        int[][] arr = new int[3][3]; // rows and columns
+
+        //arr3 = arr1 + arr2;
+
+        arr[0][0] = 10;
+        arr[0][1] = 10;
+        arr[0][2] = 10;
+        arr[1][0] = 10;
+        arr[1][1] = 10;
+        arr[1][2] = 10;
+        arr[2][0] = 10;
+        arr[2][1] = 10;
+        arr[2][2] = 10;
+
+        System.out.println(arr[0][0]);
+
+
+        // add two 2d arrays , 3x3
+        // create 1 char array
+
+    }
+}
